f0c22e9c5927f4a858d8559fe8f6e0f8a068b469
lunacabg <l.cabellos.2023@alumnos.urjc.es>
2024-02-16 15:20:46 +0100

    ---

--- S06/exercices/e4.py
+++ S06/exercices/e4.py
@@ -1,4 +1,33 @@
 import termcolor
+class Seq:
+    def __init__(self, strbases):
+        self.strbases = strbases
+        print("New list created!")
 
-termcolor.cprint(list1, 'blue')
-termcolor.cprint(list2, 'yellow')
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        return len(self.strbases)
+
+def generate_seqs(pattern, number):
+    list = []
+    for i in range(1, number + 1):
+        list.append(Seq(pattern * i))
+    return list
+
+def print_seqs(seq_list):
+    n = 0
+    for seq in seq_list:
+        termcolor.cprint(("Sequence", n, ": (Length:", seq.len(), ")", seq), 'blue')
+        n += 1
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
