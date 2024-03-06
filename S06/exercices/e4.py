aced3c051ad4e4c79821228842a2e0a030da8998
lunacabellos <l.cabellos.2023@alumnos.urjc.es>
2024-03-06 13:16:38 +0100

    New!

--- S06/exercices/e4.py
+++ S06/exercices/e4.py
@@ -1,25 +1,44 @@
-from seq01 import *
 import termcolor
 
 
-def generate_seqs(pattern, number):
-    seq_list = []
-    for i in range(1, number + 1):
-        seq_list.append(Seq(pattern * i))
-    return seq_list
+class Seq:
+
+    def __init__(self, strbases=None):
+        if type(strbases) == str and strbases.count('A') + strbases.count('C') + strbases.count('G') + strbases.count(
+                'T') == len(strbases):
+            print("New sequence created!")
+            self.strbases = strbases
+        else:
+            print('INCORRECT Sequence detected')
+            self.strbases = 'ERROR'
+
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        return len(self.strbases)
 
 
 def print_seqs(seq_list, color):
-    for s in seq_list:
-        termcolor.cprint(f"Sequence {seq_list.index(s)}: (Length: {s.len()}) {s}", color)
+    seq_n = len(seq_list)
+    for n in seq_list:
+        termcolor.cprint('Sequence ' + str(seq_list.index(n)) + ': ' + '(Length: ' + str(len(str(n))) + ') ' + str(n),
+                         color)
+
+
+def generate_seqs(pattern, number):
+    list_of_patterns = []
+    for n in range(number):
+        list_of_patterns.append(Seq(pattern + pattern * n))
+    return list_of_patterns
 
 
 seq_list1 = generate_seqs("A", 3)
 seq_list2 = generate_seqs("AC", 5)
 
-termcolor.cprint("List 1:", 'blue')
+print("List 1:")
 print_seqs(seq_list1, 'blue')
 
 print()
-termcolor.cprint("List 2:", 'green')
+print("List 2:")
 print_seqs(seq_list2, 'green')
