156ec1cc0842fed6ad484f09de24d2be2e8a9db1
lunacabellos <l.cabellos.2023@alumnos.urjc.es>
2024-03-11 13:50:27 +0100

    New!

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -1,5 +1,10 @@
+from termcolor import termcolor
+
 from P01 import Seq1
 import socket
+
+from Seq1 import Seq
+
 
 class Server:
     def __init__(self):
@@ -41,6 +46,81 @@ class Server:
 
     def calculate_response(self, msg):
         if msg.startswith("PING"):
-            return "OK!"
+            termcolor.cprint("PING command!", "green")
+            return "OK!\n"
+
+        elif msg.startswith("GET"):
+            termcolor.cprint("GET", 'green')
+            get = self.get_function(msg)
+            print(get)
+            return get
+
+
+        elif msg.startswith("INFO"):
+            termcolor.cprint("INFO", 'green')
+            info = self.info_function(msg)
+            print(info)
+            return info
+
+        elif msg.startswith("COMP"):
+            termcolor.cprint("COMP", 'green')
+            comp = self.comp_function(msg)
+            print(comp)
+            return comp
+
+        elif msg.startswith("REV"):
+            termcolor.cprint("REV", 'green')
+            rev = self.rev_function(msg)
+            print(rev)
+            return rev
+
+        elif msg.startswith("GENE"):
+            termcolor.cprint("REV", 'green')
+            gene = self.gene_function(msg)
+            print(gene)
+            return gene
+
+    def get_function(self, msg):
+        which_gene_to_send = msg.split(" ")
+        gene_to_send = which_gene_to_send[1]
+        genes = ['ADA', 'FRAT1', 'FXN', 'U5', 'RNU6_269P']
+        i = 0
+        for g in genes:
+            if str(i) == gene_to_send:
+                seq = Seq(g)
+                return seq.read_fasta(g)
+            i += 1
+
+    def info_function(self, msg):
+        gene = msg.split(" ")
+        gene = gene[1]
+        seq = Seq(gene)
+        length = f"Total length: {seq.len()}"
+        c_a = f"\nA:{seq.count_base('A')} ({seq.count_base('A') / seq.len() * 100}%)"
+        c_c = f"\nC:{seq.count_base('C')} ({seq.count_base('C') / seq.len() * 100}%)"
+        c_g = f"\nG:{seq.count_base('G')} ({seq.count_base('G') / seq.len() * 100}%)"
+        c_t = f"\nT:{seq.count_base('T')} ({seq.count_base('T') / seq.len() * 100}%)"
+        return f"Sequence: {seq} \n{length} {c_a}, {c_c}, {c_g}, {c_t}"
+
+    def comp_function(self, msg):
+        seq = msg.split(" ")
+        seq = seq[1]
+        seq = Seq(seq)
+        comp = seq.complement()
+        return comp
+
+    def rev_function(self, msg):
+        seq = msg.split(" ")
+        seq = seq[1]
+        seq = Seq(seq)
+        rev = seq.reverse()
+        return rev
+
+    def gene_function(self, msg):
+        which_gene_to_send = msg.split(" ")
+        gene_to_send = which_gene_to_send[1]
+        seq = Seq()
+        return seq.read_fasta(gene_to_send)
+
 
 object = Server()
