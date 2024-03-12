0504f0550a17d0b3eaaf651f231eb69508b70b75
lunacabg <l.cabellos.2023@alumnos.urjc.es>
2024-03-12 17:05:59 +0100

    ---

--- Guess the number/Game.py
+++ Guess the number/Game.py
@@ -1,49 +1,56 @@
 
 import socket
+import random
 
-from Seq1 import Seq
+class NumberGuesser:
+    def __init__(self, secret_number):
+        self.secret_number = secret_number
+        self.attempts = []
 
-
-class Server:
-    def __init__(self):
-
-        # Configure the Server's IP and PORT
-        PORT = 8080
-        IP = "127.0.0.1" # it depends on the machine the server is running
-
-        # create an INET, STREAMing socket
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            serversocket.bind((IP, PORT))
-            # become a server socket
-            # MAX_OPEN_REQUESTS connect requests before refusing outside connections
-            serversocket.listen()
-
-            while True:
-                # accept connections from outside
-                print(f"Waiting for clients at {IP}, {PORT}")
-                (clientsocket, address) = serversocket.accept()
-
-                # Read the message from the client, if any
-                msg = clientsocket.recv(2048).decode("utf-8")
-                rsp = self.calculate_response(msg)
-                print("Message from client: {}".format(msg))
-
-                # Send the message
-                send_bytes = str.encode(rsp)
-                # We must write bytes, not a string
-                clientsocket.send(send_bytes)
-                clientsocket.close()
-
-        except socket.error:
-            print("Problems using ip {} port {}. Is the IP correct? Do you have port permission?".format(IP, PORT))
-
-        except KeyboardInterrupt:
-            print("Server stopped by the user")
-            serversocket.close()
+    def guess(self, msg):
+        msg = int(msg)
+        if msg == self.secret_number:
+            hint = "You guessed the secret number after x tries:)"
+        elif msg < self.secret_number:
+            hint = "The secret number is higher"
+        elif msg > self.secret_number:
+            hint = "The secret number is lower"
+        self.attempts.append(msg)
+        return hint
 
 
-    def calculate_response(self, msg):
-        if msg.startswith("PING"):
-            termcolor.cprint("PING command!", "green")
-            return "OK!\n"
+PORT = 8081
+IP = "212.128.255.88" # it depends on the machine the server is running
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    serversocket.bind((IP, PORT))
+    serversocket.listen()
+    flag = True
+    while flag:
+        print(f"Waiting for clients at {IP}, {PORT}")
+        (clientsocket, address) = serversocket.accept()
+
+        print(f"Client connected, number is being generated!")
+        secret_number = random.randint(1, 100)
+        print("The secret number is:", secret_number)
+        print(f"Random number generated, please, enter your try")
+        # Read the message from the client, if any
+        msg = clientsocket.recv(2048).decode("utf-8")
+        print("Message from client: {}".format(msg))
+        ng = NumberGuesser(secret_number)
+        rsp = ng.guess(msg)
+        # Send the message
+        send_bytes = str.encode(rsp)
+        # We must write bytes, not a string
+        clientsocket.send(send_bytes)
+        clientsocket.close()
+
+except socket.error:
+    print("Problems using ip {} port {}. Is the IP correct? Do you have port permission?".format(IP, PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    serversocket.close()
+
+
+
